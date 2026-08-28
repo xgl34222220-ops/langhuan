9ce231dc54f7c765053dee19eefe99fc3e83ae83
xgl34222220-ops <xgl34222220@gmail.com>
2026-08-28 16:37:12 +0800

    chore: bump to 0.20.1 alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.xiguli.langhuan"
         minSdk = 28
         targetSdk = 36
-        versionCode = 22
-        versionName = "0.20.0-alpha01"
+        versionCode = 23
+        versionName = "0.20.1-alpha01"
     }
 
     buildFeatures { compose = true }
