a3eb449ad12442a8191d642cc2c844430859f5e4
xgl34222220-ops <xgl34222220@gmail.com>
2026-08-29 21:47:19 +0800

    feat: initialize built-in reference library on app start

--- app/src/main/java/com/xiguli/langhuan/LanghuanApplication.kt
+++ app/src/main/java/com/xiguli/langhuan/LanghuanApplication.kt
@@ -1,9 +1,15 @@
 package com.xiguli.langhuan
 
 import android.app.Application
+import com.xiguli.langhuan.engine.BuiltInReferenceLibraryInstaller
 import com.xiguli.langhuan.engine.ChapterRunRuntime
 
 class LanghuanApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        BuiltInReferenceLibraryInstaller.install(this)
+    }
+
     val chapterRunRuntime: ChapterRunRuntime by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
         ChapterRunRuntime(this)
     }
