83b1e44eae7cbdf3df53c45b9c4278482193be9c
xgl34222220-ops <xgl34222220@gmail.com>
2026-08-29 11:21:20 +0800

    chore: bump run center build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.xiguli.langhuan"
         minSdk = 28
         targetSdk = 36
-        versionCode = 64
-        versionName = "0.26.6-alpha01"
+        versionCode = 65
+        versionName = "0.26.7-alpha01"
     }
 
     buildFeatures { compose = true }
