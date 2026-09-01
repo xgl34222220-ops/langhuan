495508a3988ce25d4c5bdea9f8b41ec18528e594
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-01 11:35:20 +0800

    feat: wire story space panel into book experience

--- app/src/main/java/com/xiguli/langhuan/ui/BookExperienceV4.kt
+++ app/src/main/java/com/xiguli/langhuan/ui/BookExperienceV4.kt
@@ -110,7 +110,7 @@ fun BookExperienceV4(
                     onAgent = onOpenAgent,
                     onRunCenter = onOpenRunCenter,
                 )
-                BookTabV4.STORY -> StoryPlayPanelV12(
+                BookTabV4.STORY -> StoryPlayPanelV13(
                     book = book,
                     libraryState = state,
                     aiReady = studioState.provider.ready,
