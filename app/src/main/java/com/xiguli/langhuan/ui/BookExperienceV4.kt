db78f41231f1d0338f176ff660870095375a035c
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-01 12:52:27 +0800

    feat: wire story role takeover into book experience

--- app/src/main/java/com/xiguli/langhuan/ui/BookExperienceV4.kt
+++ app/src/main/java/com/xiguli/langhuan/ui/BookExperienceV4.kt
@@ -110,7 +110,7 @@ fun BookExperienceV4(
                     onAgent = onOpenAgent,
                     onRunCenter = onOpenRunCenter,
                 )
-                BookTabV4.STORY -> StoryPlayPanelV15(
+                BookTabV4.STORY -> StoryPlayPanelV16(
                     book = book,
                     libraryState = state,
                     aiReady = studioState.provider.ready,
