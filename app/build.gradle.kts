72e042d11ef54c99ec0eb1e0ff83d86d1b87b3f8
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-01 23:35:44 +0800

    build: bump reader appearance V9 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.xiguli.langhuan"
         minSdk = 28
         targetSdk = 36
-        versionCode = 78
-        versionName = "0.28.0-alpha09"
+        versionCode = 79
+        versionName = "0.28.0-alpha10"
     }
 
     buildFeatures { compose = true }
