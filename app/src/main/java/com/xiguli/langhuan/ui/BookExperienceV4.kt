33ef47b1fd5ab57e9b174a14cc331e7cb2e7ec17
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-01 09:00:02 +0800

    feat: route story tab through tavern director experience

--- app/src/main/java/com/xiguli/langhuan/ui/BookExperienceV4.kt
+++ app/src/main/java/com/xiguli/langhuan/ui/BookExperienceV4.kt
@@ -110,7 +110,7 @@ fun BookExperienceV4(
                     onAgent = onOpenAgent,
                     onRunCenter = onOpenRunCenter,
                 )
-                BookTabV4.STORY -> StoryPlayPanelV6(
+                BookTabV4.STORY -> StoryPlayPanelV7(
                     book = book,
                     libraryState = state,
                     aiReady = studioState.provider.ready,
