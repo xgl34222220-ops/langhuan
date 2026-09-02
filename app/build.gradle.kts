ebb1afc8c1bf5b6741dad3844bd14942237e3801
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-02 19:28:50 +0800

    chore: bump full shell redesign to ui4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.xiguli.langhuan"
         minSdk = 28
         targetSdk = 36
-        versionCode = 89
-        versionName = "0.28.0-alpha12-ui3"
+        versionCode = 90
+        versionName = "0.28.0-alpha12-ui4"
     }
 
     buildFeatures { compose = true }
