108e3e51d3e739e5d01fdd57e0459b5539b0c585
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-04 18:40:06 +0800

    fix(ui): import row weight for component kit

--- app/src/main/java/com/xiguli/langhuan/ui/design/LanghuanComponentKitV3.kt
+++ app/src/main/java/com/xiguli/langhuan/ui/design/LanghuanComponentKitV3.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
